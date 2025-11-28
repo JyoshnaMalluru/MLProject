117a6f7836e303e0b3a409a7b9a4f2cda60ce905
Jyoshna Malluru <jyoshna1595@gmail.com>
2025-11-28 15:36:40 +0530

    add changes

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,14 @@
 import sys
-import logging
+from .logger import logging
+
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name [{0}] line number: [{1}] error message".format(
+    error_message = "Error occurred in python script name [{0}] line number: [{1}] error message: [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
     return error_message
+
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
@@ -14,9 +16,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Divided by zero")
-        raise CustomException(e,sys)

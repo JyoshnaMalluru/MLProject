333a842538579b584b20826d33148eb9cda3269d
Jyoshna Malluru <jyoshna1595@gmail.com>
2026-02-05 17:27:39 +0530

    logger and exception

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import find_packages, setup
 from typing import List
 HYPHEN_E_DOT = '-e .'
-def get_requirements(file_path: str) -> List[str]:
-    '''This function will return the list of requirements'''
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
     requirements=[]
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 logging.basicConfig(
@@ -13,3 +13,6 @@ logging.basicConfig(
     level=logging.INFO,
 
 )
+
+# if __name__=="__main__":
+#     logging.info("Logging has started")
